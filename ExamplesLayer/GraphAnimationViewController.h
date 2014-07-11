8a42ff0a72106cf9228f9c0bb5af6f0773fbc47b
Edgar <hunk@fury.local>
2014-07-11 14:21:26 -0500

    update gitignore

---
+++ ExamplesLayer/GraphAnimationViewController.h
@@ -0,0 +1,13 @@
+//
+//  GraphAnimationViewController.h
+//  ExamplesLayer
+//
+//  Created by Edgar on 7/11/14.
+//  Copyright (c) 2014 hunk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphAnimationViewController : UIViewController
+
+@end
